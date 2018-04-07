06f6837439925cdbf071e968474c86079a42da6b
jealob <jealob@gmail.com>
2018-04-06 23:59:14 -0500

    update game and functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,10 +7,46 @@ $(document).ready(function () {
     let currentNumber;
     let wins = 0;
     let losses = 0;
+    var x = document.createElement("audio");
 
-    // startGame initializes the global variables. Assign random numbers to both targetNumber and the crystal.
+    // Click event check whether or not the current score is equals target score and run the program accordingly.
+    $(".crystals").on("click", ".game-col", function () {
+        // currentNumber < targetNumber makes sure that execution of accidental click event is unresponsive during end of round.
+        if (currentNumber < targetNumber) {
+            gameSoundPlay("assets/images/ping.mp3");
+            // picks the value on the click event and compare with crystal object to get the value of the property clicked.
+            currentNumber += crystals[$(this).attr('value')];
+            $("#current-score").html(currentNumber);
+            if (currentNumber === targetNumber) {
+                wins++;
+                gameSoundPlay("assets/images/cheering.mp3");
+                setTimeout(function(){
+                    alert("You Win!!!");
+                    nextRound();  
+                }, 500);  
+            }
+            else if (currentNumber > targetNumber){
+                losses++;
+                gameSoundPlay("assets/images/fail.mp3");
+                setTimeout(function(){
+                    alert("You Lose!");
+                    nextRound();
+                }, 500);                    
+            }
+        }
+    });
+
+    $("body").on("click", "#new-game", function(){
+        newGame();
+    });
+
+    $("#wins").text(wins);
+    $("#losses").text(losses);
+    
+
+    // nextRound initializes the global variables. Assign random numbers to both targetNumber and the crystal.
     // the value of the crystals are stored in the an object crystal for easy access and manipulation between DOM events and the crystals object properties
-    function startGame() { 
+    function nextRound() { 
         currentNumber = 0;
         targetNumber = getRandomInt(19, 120);
         for (let property in crystals) {
@@ -24,35 +60,27 @@ $(document).ready(function () {
         console.log(crystals);
     }
 
-    // Click event check whether or not the current score is equals target score and run the program accordingly.
-    // 
-    $(".crystals").on("click", ".game-col", function () {
-        // picks the value on the click event and compare with crystal object to get the value of the property clicked.
-        currentNumber += crystals[$(this).attr('value')]; 
-        // debugger;
-        $("#current-score").html(currentNumber);
-        if (currentNumber === targetNumber) {
-            wins++;
-            // setTimeout(function(){
-                startGame();
-                alert("You Win!!!");    
-            // }, 500);
-        }
-        else if (currentNumber > targetNumber) {
-            losses++;
-            // setTimeout(function(){
-                startGame();
-                alert("You Lose!");
-            // }, 500);
-                    
-        }
-        $("#wins").text(wins);
-        $("#losses").text(losses);
-    });
-    
+    function newGame() {
+        wins = 0;
+        losses = 0;
+        // gameSoundPause();
+        nextRound();
+    }
+
 //  Function getRandomInt returns random number between two numbers max and min
     function getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-    startGame();
+
+    function gameSoundPlay(s) {
+        x.setAttribute("src", s);
+        x.play();
+    }
+
+    // Stop existing sound the start new game 
+    // function gameSoundPause() {
+    //     x.pause(s)
+    // }
+
+    nextRound();
 });
